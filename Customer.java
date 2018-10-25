5673b8cfbccc941d7d75992e092a2cc9fffcb7c5
clbokea <clbo@kea.dk>
2018-10-25 09:42:22 +0200

    første time

--- Customer.java
+++ Customer.java
@@ -1,6 +1,8 @@
 public class Customer {
 
    private String name;
+   private Account[] accounts = new Account[3]; 
+   
    
    public Customer(String name){
       this.name = name;
@@ -9,4 +11,13 @@ public class Customer {
    public String getName(){
       return name;
    }
-}
+   
+   public void addAccount(Account ac){
+      accounts[0] = ac;
+   }
+   
+   public Account[] getAccounts(){
+      return accounts;
+   }
+   
+   }
--- MainFile.java
+++ MainFile.java
@@ -5,11 +5,20 @@ public class MainFile{
       Customer customer = new Customer("James");
       Account account = new Account(123456789);
       
-      account.deposit(10000.00);
+      account.deposit(1999990000.00);
       String checkBal = account.checkBalance(customer);
-      System.out.println(checkBal);
+      
+      
+      customer.addAccount(account);
+      
+      // få fat i en liste af kundens kontoer
+      
+      Account[] xxx = customer.getAccounts();
+      
+      System.out.println(xxx[0].checkBalance(customer));
+       System.out.println(xxx[1].checkBalance(customer));
+       System.out.println(xxx[2].checkBalance(customer));
+      
       
    }
-
-
 }
